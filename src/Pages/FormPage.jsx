069793efde91d1c0d2a83ac8fd0c37d43944e8cb
Mikel Jimenez <majc1907@gmail.com>
2024-05-13 17:48:25 +0200

    eighth commit

--- src/Pages/FormPage.jsx
+++ src/Pages/FormPage.jsx
@@ -1,9 +1,29 @@
 
 function FormPage(){
-   
+    const newFilm = {
+        name: "",
+        year: "",
+        overview: "",
+        image: ""
+    }
+    const handleSubmit= (event)=>{
+        event.preventDefault()
+    }
     return(
         <>
-
+   <div>
+            <form className="form-content" onChange={handleSubmit} action="">
+<label htmlFor="Name">Name </label>
+<input type="text" name="name" />
+<label htmlFor="year">Year </label>
+<input type="number" name="number" />
+<label htmlFor="Message">Message </label>
+<input type="text" name="message"/>
+<label htmlFor="Image">Mail</label>
+<input type="image" name="mail" />
+<button>Send</button>
+            </form>
+        </div>
         </>
     )
 }
