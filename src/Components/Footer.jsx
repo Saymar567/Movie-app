b2c1b400b955a7a2485aaf326df7da7a8b4a65e2
Mikel Jimenez <majc1907@gmail.com>
2024-05-14 13:33:30 +0200

    second-day-seventh-commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = ()=>{
     
     return (
             <footer className="footer">
+               <div>
                 <img src="/src/images/icons8-twitter-25.png" alt="twitter-logo" /> 
                 <img src="/src/images/icons8-facebook-25.png" alt="" />
                 <img src="/src/images/icons8-instagram-25.png" alt="" />
+                </div> 
        Video-Club Santutxu ©️ {newDate.getFullYear()}
      
        <Link to="/contactForm"><button>Contact me</button></Link>
