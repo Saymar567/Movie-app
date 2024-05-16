365af288bf57eb6435656ba002d59ec46ce4432e
Mikel Jimenez <majc1907@gmail.com>
2024-05-16 20:10:41 +0200

    fourth-day-tenth-commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import Twitter from "../Images/icons8-twitter-25.png"
+
 const Footer = ()=>{
     const newDate = new Date();
     
     return (
             <footer className="footer">
                <div>
-                <a href="https:/twitter.com" target="_blank"><img src="/src/images/icons8-twitter-25.png" alt="twitter-logo" /> </a>
+                <a href="https:/twitter.com" target="_blank"><img src={Twitter} alt="twitter-logo" /> </a>
                <a href="https:/facebook.com" target="_blank"> <img src="/src/images/icons8-facebook-25.png" alt="" /></a>
                 <a href="https:/instagram.com"  target="_blank"><img src="/src/images/icons8-instagram-25.png" alt="" /></a>
                 </div> 
