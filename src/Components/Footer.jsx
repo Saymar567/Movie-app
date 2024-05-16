f91df6f39bc1cc42f6bd5c94300e91db052ad74d
Mikel Jimenez <majc1907@gmail.com>
2024-05-16 20:12:51 +0200

    fourth-day-eleventh-commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import Twitter from "../Images/icons8-twitter-25.png"
+import Facebook from "../Images/icons8-facebook-25.png"
+import Instagram from "../Images/icons8-instagram-25.png"
 
 const Footer = ()=>{
     const newDate = new Date();
@@ -8,8 +10,8 @@ const Footer = ()=>{
             <footer className="footer">
                <div>
                 <a href="https:/twitter.com" target="_blank"><img src={Twitter} alt="twitter-logo" /> </a>
-               <a href="https:/facebook.com" target="_blank"> <img src="/src/images/icons8-facebook-25.png" alt="" /></a>
-                <a href="https:/instagram.com"  target="_blank"><img src="/src/images/icons8-instagram-25.png" alt="" /></a>
+               <a href="https:/facebook.com" target="_blank"> <img src={Facebook} alt="" /></a>
+                <a href="https:/instagram.com"  target="_blank"><img src={Instagram} alt="" /></a>
                 </div> 
        Video-Club Santutxu ©️ {newDate.getFullYear()}
      
